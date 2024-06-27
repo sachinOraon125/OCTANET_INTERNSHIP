c0d256fff7224ce93d7566bad0249dc3ef3bdbd3
sachinOraon125 <sachinoraon440@gmail.com>
2024-06-27 09:12:58 +0530

    Add files via upload

---
+++ Task 1_Landing Page/index.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disbledonInteraction: false,
+    },
+  });
+  
